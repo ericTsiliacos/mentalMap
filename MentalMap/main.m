03ef8c3db081fced4f65fa93f55be42c5b917d0d
Eric Tsiliacos <erictsiliacos@gmail.com>
2014-09-10 23:06:06 -0700

    Initial Commit

---
+++ MentalMap/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MentalMap
+//
+//  Created by Eric Tsiliacos on 9/10/14.
+//  Copyright (c) 2014 eric. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
