c99c325a69ea22646076a3e57e3bbe66e57f9df9
Eric Tsiliacos <erictsiliacos@gmail.com>
2014-09-10 23:24:45 -0700

    Cocoapods and Cedar to project

---
+++ MentalMapTests/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MentalMapTests
+//
+//  Created by Eric Tsiliacos on 9/10/14.
+//  Copyright (c) 2014 eric. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <Cedar-iOS/Cedar-iOS.h>
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, @"CedarApplicationDelegate");
+    }
+}
---
+++ Pods/Pods-MentalMapTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MentalMapTests : NSObject
+@end
+@implementation PodsDummy_Pods_MentalMapTests
+@end
